7545f9bfcab22bffdef48fab248a1f300df69cee
Agustín <tomasmunizvozza@gmail.com>
2025-07-05 14:03:49 -0300

    actividad Práctica 1

---
+++ act_práctica_1/script.js
@@ -0,0 +1,11 @@
+// Número
+let numero = 42;
+console.log("El número es:", numero);
+
+// Cadena de texto
+let texto = "Hola, mundo!";
+console.log("el texto es:", texto);
+
+// Booleano
+let esValido = true;
+console.log("¿Es válido?", esValido);
